67f148b716c9b5ee98c6afeaf2677bd2dd4c2b4a
DESKTOP-U0ARJOJ\outman <812020728@qq.com>
2022-02-19 17:03:52 +0800

    feat(Chapter 1.2): 初识输入输出
    - 命名空间可以避免重定义名字
    
    向流steam写:
    
    - 用 (<<) 运算符不停的将右边的值塞进左边的对象中
    
    - std::endl 的作用: 结束当前行，并清除缓存区
    
    从流steam读:
    
    - 用 (>>) 运算符将采集到的值不停的往右边塞

--- Chapter 1/README.md
+++ Chapter 1/README.md
@@ -1,4 +1,6 @@
-# 练习1.1
+# Chapter 1.1
+
+## 练习1.1
 
 - 查阅编译器文档，确定它的文件命名约束
 
@@ -29,7 +31,7 @@ echo $?
 # 0
 ```
 
-# 练习1.2
+## 练习1.2
 
 把return改成-1，观察系统怎么处理main返回的错误标识
 
@@ -45,3 +47,21 @@ echo $?
 原因：linux程序退出时有个错误码 Exit Codes 范围是 0-255，-1从后往前就是255
 
 Depending on our shell, exit code 255 might mean that the returned exit code is outside of the 0-255 range.
+
+# Chapter 1.2
+
+### 向流steam写:
+
+- 用 (<<) 运算符不停的将右边的值塞进左边的对象中
+
+- std::endl 的作用: 结束当前行，并清除缓存区
+
+- 命名空间可以避免重定义名字
+
+### 从流steam读:
+
+- 用 (>>) 运算符将采集到的值不停的往右边塞
+
+## 练习题
+
+略
--- Chapter 1/main.cpp
+++ Chapter 1/main.cpp
@@ -1,5 +1,25 @@
+#include <iostream>
 int main()
 {
+	// std::cout << "Enter two number:" << std::endl;
+	// int x = 0, y = 0;
+	// std::cin >> x >> y;
+	// std::cout << "The sum of " << x << " and " << y
+	// 		  	 << " is " << x + y << std::endl;
+
+	/*
+		练习1.3输出hello world
+		std::cout << "Hello World" << std::endl;
+	*/
+
+	/*
+		练习1.4 两数相乘
+		std::cout << "Enter two number:" << std::endl;
+		int x = 0, y = 0;
+		std::cin >> x >> y;
+		std::cout << "The mul of " << x << " and " << y
+				  << " is " << x * y << std::endl;
+	*/
+
 	return 0;
-	// return -1;
 }
